a59834908612570234529964f8638e1db2c1a90f
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2024-04-11 16:35:57 +0200

    home styled expect modal

--- src/App.svelte
+++ src/App.svelte
@@ -5,17 +5,63 @@
   let isAddFormOpen = false;
 </script>
 
-<main>
+<h1>VIRTUEOSO</h1>
+<section class="list-container">
+  <h2>Virtues</h2>
   <ul>
-    {#each $listElements as name}
-      <ListElement {name} />
-    {/each}
+    {#if $listElements.length > 0}
+      {#each $listElements as name}
+        <ListElement {name} />
+      {/each}
+    {:else}
+      <span class="message">No virtues to show</span>
+    {/if}
   </ul>
-  <button on:click={() => (isAddFormOpen = true)}>Add virtue</button>
-</main>
+  <button
+    class="button add-virtue-button"
+    on:click={() => (isAddFormOpen = true)}>Add virtue</button
+  >
+</section>
 {#if isAddFormOpen}
   <AddForm />
 {/if}
 
 <style>
+  .list-container {
+    /* display: flex;
+    flex-direction: column; */
+    /* align-items: center; */
+    width: 80vw;
+    max-width: 20rem;
+  }
+
+  .list-container h2 {
+    margin-bottom: 0.2em;
+  }
+
+  .list-container ul {
+    margin-bottom: 1em;
+  }
+
+  .list-container .message {
+    display: block;
+    margin-top: 1em;
+    margin-bottom: 2em;
+  }
+
+  h1 {
+    font-family: "Cinzel", serif;
+    font-weight: 430;
+    text-align: center;
+    margin-top: 1em;
+    margin-bottom: 1em;
+  }
+
+  .add-virtue-button {
+    background-color: var(--grey-attention);
+    width: min-content;
+    white-space: nowrap;
+    display: block;
+    margin: auto;
+  }
 </style>
--- src/lib/ListElement.svelte
+++ src/lib/ListElement.svelte
@@ -3,8 +3,23 @@
 </script>
 
 <li>
-  {name}
+  <span>
+    {name}
+  </span>
+  <button class="button list-element-button">View</button>
 </li>
 
 <style>
+  li {
+    border: 1px solid var(--text-color);
+    font-size: 0.938rem;
+    padding: 0.8em 0.9em;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .list-element-button {
+    padding: 0.2em 0.5em;
+  }
 </style>
